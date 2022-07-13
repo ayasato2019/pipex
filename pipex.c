0cb75d3762dd9de1a1abdf6fb9bc7141e4a3a92c
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-07-13 10:19:06 +0900

    test2

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: satouaya <satouaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 16:11:01 by aysato            #+#    #+#             */
-/*   Updated: 2022/07/12 23:15:26 by satouaya         ###   ########.fr       */
+/*   Updated: 2022/07/13 10:18:59 by satouaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,54 @@
 #include <stdlib.h>
 
 #define LOG_FILE_PATH "./test.log"
-int main(void){
-    int fd,size;
-    char buf[128];
+// int main(void){
+//     int fd,size;
+//     char buf[128];
 
-    fd = open(LOG_FILE_PATH, (O_RDONLY), 0664);
+//     fd = open(LOG_FILE_PATH, (O_RDONLY), 0664);
 
-    if(fd < 0)
-        return -1;
-    close(fd);
-    return 0;
-}
-
-// int main(int argc, char *argv[])
-// {
-//     int pipefd[2];
-//     pid_t cpid;
-//     char buf;
-    
-//     if (argc != 2)
-//     {
-//         fprintf(stderr,"Usage:%s<string>\n",argv[0]);
-//         exit(EXIT_FAILURE);
-//     }
-    
-//     if (pipe(pipefd) == -1) {
-//         perror("pipe");
-//         exit(EXIT_FAILURE);
-//     }
-    
-//     cpid = fork();
-//     if (cpid == -1) {
-//         perror("fork");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     if (cpid == 0) {
-//         //小プロセルがパイプから読み込む
-//         close(pipefd[1]);//使わないwrite側はクローズする
-//         while (read(pipefd[0],&buf,1) > 0)
-//             write(STDOUT_FILENO,&buf,1);
-//         write(STDOUT_FILENO, "\n", 1);
-//         close(pipefd[0]);
-//         _exit(EXIT_SUCCESS);
-//     } else {//親プロセスはargv[1]をパイプへ書き込む
-//         close(pipefd[0]);//使用しないread側はクローズする
-//         write(pipefd[1],argv[1],strlen(argv[1]));//読み込み側がEOFに出会う
-//         close(pipefd[1]);//子プロセスを待つ
-//         wait(NULL);
-//         exit(EXIT_SUCCESS);
-//     }
+//     if(fd < 0)
+//         return -1;
+//     close(fd);
+//     return 0;
 // }
+
+int main(int argc, char *argv[])
+{
+    int pipefd[2];
+    pid_t cpid;
+    char buf;
+    
+    if (argc != 2)
+    {
+        fprintf(stderr,"Usage:%s<string>\n",argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cpid == 0) {
+        //小プロセルがパイプから読み込む
+        close(pipefd[1]);//使わないwrite側はクローズする
+        while (read(pipefd[0],&buf,1) > 0)
+            write(STDOUT_FILENO,&buf,1);
+        write(STDOUT_FILENO, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+    } else {//親プロセスはargv[1]をパイプへ書き込む
+        close(pipefd[0]);//使用しないread側はクローズする
+        write(pipefd[1],argv[1],strlen(argv[1]));//読み込み側がEOFに出会う
+        close(pipefd[1]);//子プロセスを待つ
+        wait(NULL);
+        exit(EXIT_SUCCESS);
+    }
+}
