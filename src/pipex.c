9d943a1341d39cde738f6d25d613150159301ba6
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-07-27 14:49:28 +0900

    テスト

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: satouaya <satouaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 16:11:01 by aysato            #+#    #+#             */
-/*   Updated: 2022/07/27 13:45:06 by satouaya         ###   ########.fr       */
+/*   Updated: 2022/07/27 14:47:01 by satouaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,65 @@
 void	child_process(char **argv, char **envp, int *fd)
 {
 	int	fd_infile;
-	int	status;
 	int	i;
+	char    **cmd;
 
 	i = 0;
 	fd_infile = open(argv[1], O_RDONLY, 0777);
 	if (fd_infile == -1)
 		set_perror("child process infile open", EXIT_FAILURE);
-	status = dup2(fd_infile, STDIN_FILENO);
-	if (status == -1)
+	if (dup2(fd_infile, STDIN_FILENO) == -1)
 		set_perror("child process infile dup", EXIT_FAILURE);
-	status = dup2(fd[1], STDOUT_FILENO);
-	if (status == -1)
-		set_perror("child process fd[1] dup", EXIT_FAILURE);
 	close(fd[0]);
-	while(envp[i])
-	{
-		execve(envp[i], &argv[2], envp);
-		i++;
-	}
-	printf("通過確認 cp");
-	// if (execve("/bin/ls", "li -a", envp) == -1)
+	// printf("%d\n", fd_infile);
+	close(fd_infile);
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		set_perror("child process fd[1] dup", EXIT_FAILURE);
+	close(fd[1]);
+	cmd = get_command(&argv[2]);
+	envp = get_filepath(envp);
+	// printf("%d\n", fd_infile);
+	// execve("./", cmd, envp);
+	// while(envp[i])
 	// {
-	// 	perror("child process execve");
-	// 	_exit(EXIT_FAILURE);
+	// 	execve("./", cmd, envp);
+	// 	i++;
 	// }
+	if (execve("./", cmd, envp) == -1)
+	{
+		perror("child process execve");
+		_exit(EXIT_FAILURE);
+	}
 }
 
 void	parent_process(char **argv, char **envp, int *fd)
 {
 	int	fd_outfile;
-	int	status;
 	int	i;
+	char    **cmd;
 
 	i = 0;
 	fd_outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd_outfile == -1)
 		set_perror("outfile fd", EXIT_FAILURE);
-	status = dup2(fd_outfile, STDOUT_FILENO);
-	if (status == -1)
+	if (dup2(fd_outfile, STDOUT_FILENO) == -1)
 		set_perror("parent process outfile dup", EXIT_FAILURE);
-	status = dup2(fd[0], STDIN_FILENO);
-	if (status == -1)
-		set_perror("parent process fd[0] dupe", EXIT_FAILURE);
-	close(fd[1]);
-	printf("通過確認 pp");
-	while(envp[i])
-	{
-		execve(envp[i], &argv[2], envp);
-		i++;
-	}
-	// if (execve("/usr/bin/wc", "wc -l", envp) == -1)
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		set_perror("parent process fd[0] dup", EXIT_FAILURE);
+	close(fd_outfile);
+	close(fd[0]);
+	cmd = get_command(&argv[3]);
+	envp = get_filepath(envp);
+	// while(envp[i])
 	// {
-	// 	set_perror("parent process execve", EXIT_FAILURE);
+	// 	execve(envp[i], cmd, envp);
+	// 	i++;
 	// }
+	if (execve("./", cmd, envp) == -1)
+	{
+		set_perror("parent process execve", EXIT_FAILURE);
+		exit(EXIT_FAILURE);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -82,7 +87,6 @@ int	main(int argc, char **argv, char **envp)
 	if (pipe(fd) == -1)
 		set_perror("pipe", EXIT_FAILURE);
 	pid = fork();
-	printf("通過確認1");
 	if (pid == -1)
 		set_perror("fork", EXIT_FAILURE);
 	else if (pid == 0)
@@ -94,6 +98,5 @@ int	main(int argc, char **argv, char **envp)
 		waitpid(pid, &status, 0);
 		parent_process(argv, envp, fd);
 	}
-	printf("通過確認2");
 	return (0);
 }
