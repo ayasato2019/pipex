fe9f2e55bf7608e59ad7536790f38f1cb5ecce73
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-08-15 08:36:21 +0900

    途中メモ 方向修正

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: satouaya <satouaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 16:11:01 by aysato            #+#    #+#             */
-/*   Updated: 2022/08/15 08:06:09 by satouaya         ###   ########.fr       */
+/*   Updated: 2022/08/15 08:32:48 by satouaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,52 @@
 void	process_parents(pid_t *pid, char **envp)
 {
 	int		status;
-
-	if (waitpid(pid[2], &status, 0) != -1)
-	{
-		if (waitpid(pid[1], &status, 0) != -1)
-		{
-			if (waitpid(pid[0], &status, 0) != -1)
-			{
-				try_execve(envp, cmd);
-			}
-			else
-				set_perror("wait 1", EXIT_FAILURE);
-	}
-	else
-		set_perror("wait 1", EXIT_FAILURE);
 }
 
-void	process_fork(int i)
+void	process_fork(int argc, int i)
 {
 	pid_t	*pid;
 
-	pid[i] = fork();
-	while (pid[i] <= (argc - 3))
+	if (i == (argc - 3))
+		//process_parents
+	else
 	{
+		process_pipe();
+		pid = fork();
 		if (pid == 0)
-			cmd = process_1st(argv, envp, fd_pipe);
+		{
+			process_dup2();
+			process_fork(argc, i + 1);
+		}
 		else if (pid > 0)
-			process_parents(pid, envp);
+		{
+			process_dup2();
+			try_execve();
+		}	
 		else
 			set_perror("fork", EXIT_FAILURE);
-		i++;
 	}
+}
+
+int	*process_pipe(void)
+{
+	int		fd_pipe[2];
+
+	if (pipe(fd_pipe) == -1)
+		set_perror("pipe", EXIT_FAILURE);
+	else
+		return (fd_pipe);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		fd_pipe[2];
 	int		status;
 	int		i;
 	char	**cmd;
 
 	if (argc != 5)
 		set_perror("Bad arguments", EXIT_FAILURE);
-	if (pipe(fd_pipe) == -1)
-		set_perror("pipe", EXIT_FAILURE);
-	process_fork(pid_t *pid, int i);
+	process_pipe();
+	process_fork(argc, i);
 	return (0);
 }
