1255a8e203c137c6dcf6d748bbf0a5c70d446d22
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-08-24 23:49:14 +0900

    設計方針

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,21 @@
 /*   By: satouaya <satouaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 20:55:01 by satouaya          #+#    #+#             */
-/*   Updated: 2022/08/20 15:26:26 by satouaya         ###   ########.fr       */
+/*   Updated: 2022/08/24 23:48:36 by satouaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/pipex.h"
+#include "pipex.h"
 
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
-	int		status;
+	// int		status;
 	pid_t	pid;
+	int		i;
+	char	**cmd_1;
 
+	i = 0;
 	if (argc != 5)
 		set_perror("Bad arguments", EXIT_FAILURE);
 	if (pipe(fd) == -1)
@@ -26,12 +29,24 @@ int	main(int argc, char **argv, char **envp)
 	if (pid == -1)
 		set_perror("fork", EXIT_FAILURE);
 	else if (pid == 0)
-		child_process(argv, envp, fd);
+	{
+		first_process(argv, fd);
+	}
 	else
 	{
-		if (waitpid(pid, &status, 0) == -1)
-			set_perror("waitpid", EXIT_FAILURE);
-		parent_process(argv, envp, fd);
+		recursive_fork(argc, argv, envp, i);
+		if (dup2(fd[READ], STDIN_FILENO) == -1)
+			set_perror("fd[0] dup", EXIT_FAILURE);
+		close(fd[WRITE]);
+		close(fd[READ]);
+		cmd_1 = get_command(&argv[2]);
+		make_cmd_filepath(envp, cmd_1);
 	}
 	return (0);
 }
+/*
+	方針
+
+	子プロセスの中でforkを繰り返して、
+	mainの親プロセスはwaiteする
+*/
