4f78261e0ad6586eb4430930509ea9deffda21e7
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2022-08-25 14:21:37 +0900

    なぜか作動しない

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: satouaya <satouaya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 20:55:01 by satouaya          #+#    #+#             */
-/*   Updated: 2022/08/25 13:28:50 by satouaya         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:19:47 by satouaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,8 @@ int	main(int argc, char **argv, char **envp)
 	int		fd[2];
 	int		status;
 	pid_t	pid;
-	int		i;
-	char	**cmd_1;
-	int		cmd_count;
+	char	**cmd;
 
-	i = 0;
-	cmd_count = (argc - 2);
 	if (argc != 5)
 		set_perror("Bad arguments", EXIT_FAILURE);
 	if (pipe(fd) == -1)
@@ -33,7 +29,7 @@ int	main(int argc, char **argv, char **envp)
 	else if (pid == 0)
 	{
 		first_process(argv, fd);
-		recursive_fork(argc, argv, envp, i);
+		recursive_fork(argv, envp);
 	}
 	else
 	{
@@ -42,8 +38,8 @@ int	main(int argc, char **argv, char **envp)
 		close(fd[READ]);
 		if (waitpid(pid, &status, 0) == -1)
 			set_perror("waitpid", EXIT_FAILURE);
-		cmd_1 = get_command(&argv[i + 2]);
-		make_cmd_filepath(envp, cmd_1);
+		cmd = get_command(&argv[2]);
+		make_cmd_filepath(envp, cmd);
 	}
 	return (0);
 }
